484eff9eddfbb33dc8da672aea74a6e0f2f7269a
gaussianswan <srerrie@gmail.com>
2022-11-27 18:11:36 -0500

    added telegram bot keys to the load api keys

--- load_api_keys.py
+++ load_api_keys.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 from collections import namedtuple
 
-def load_alpaca_paper_trading_keys(dotenv_path: None) -> namedtuple:
+def load_alpaca_paper_trading_keys(dotenv_path: str = None) -> namedtuple:
 
     alpaca_paper_trading_keys = namedtuple(
         "AlpacaPaperTradingKeys",
@@ -16,4 +16,18 @@ def load_alpaca_paper_trading_keys(dotenv_path: None) -> namedtuple:
     secret_key = os.environ['ALPACA_PAPER_TRADING_SECRET_KEY']
     keys = alpaca_paper_trading_keys(api_key, secret_key)
 
+    return keys
+
+def load_telegram_bot_keys(dotenv_path: str = None) -> namedtuple:
+
+    telegram_bot_keys = namedtuple(
+        "TelegramBotKeys",
+        field_names=['bot_token', 'chat_id']
+    )
+
+    load_dotenv(dotenv_path=dotenv_path)
+
+    bot_token = os.environ['TELEGRAM_BOT_TOKEN']
+    chat_id = os.environ['TELEGRAM_BOT_CHANNEL_ID']
+    keys = telegram_bot_keys(bot_token=bot_token, chat_id=chat_id)
     return keys
