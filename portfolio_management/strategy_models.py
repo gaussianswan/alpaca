f9897bcc4455155a57b0fb1ff1a025e3999aabd7
gaussianswan <srerrie@gmail.com>
2022-12-23 17:52:32 -0500

    added dataclasses for strategy pos and trades

---
+++ portfolio_management/strategy_models.py
@@ -0,0 +1,47 @@
+from alpaca.trading.models import Position, Order
+from uuid import UUID
+from dataclasses import dataclass, asdict
+from typing import List
+
+@dataclass
+class StrategyPosition(Position):
+    strategy_id: str
+
+    @classmethod
+    def from_position(cls, position: Position, strategy_id: str):
+        return cls(
+            strategy_id,
+            **asdict(position)
+        )
+
+@dataclass
+class StrategyTrade(Order):
+    strategy_id: str
+
+    @classmethod
+    def from_order(cls, order: Order, strategy_id: str):
+        return cls(
+            strategy_id,
+            **asdict(order)
+        )
+
+@dataclass
+class StrategyTradeHistory:
+    trade_history = List[StrategyTrade]
+
+@dataclass
+class StrategyPositionHistory:
+    position_history = List[StrategyPosition]
+
+
+
+
+
+
+
+
+
+
+
+
+
